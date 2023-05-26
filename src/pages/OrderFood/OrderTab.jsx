6a2aedd2daac20aae6a4273d7c114483e8bcac4e
Robbani571 <gr77rubel@gmail.com>
2023-05-26 21:05:24 +0600

    testimonials eddited

--- src/pages/OrderFood/OrderTab.jsx
+++ src/pages/OrderFood/OrderTab.jsx
@@ -1,15 +1,42 @@
 import MenuCard from "../Shared/MenuCard/MenuCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 
 
-const OrderTab = ({menu}) => {
+const OrderTab = ({ menu }) => {
+
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
+
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {menu.map((item) => (
+        <div>
+
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {menu.map((item) => (
                             <MenuCard
                                 key={item._id}
                                 item={item}
                             ></MenuCard>
                         ))}
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
